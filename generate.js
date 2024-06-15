37445984880853b98f2666f5aae65579e1b2c3d2
Tracy Li <teresa.arciaga@gmail.com>
2024-06-15 16:41:25 +0800

    Update generate.js

--- generate.js
+++ generate.js
@@ -7,7 +7,7 @@ async function signMessageWithEthers(privateKey, message) {
 }
 
 const message = "Hello, Ethereum!";
-const privateKey = 'dc1a88872b5c965004cebd2450be493b701937118aac41040b8ad4c5dba7ee70'; // Make sure to provide your private key with or without the '0x' prefix
+const privateKey = ''; // Make sure to provide your private key with or without the '0x' prefix
 signMessageWithEthers(privateKey, message).then(signature => {
     console.log(`Signature: ${signature}`);
 });
